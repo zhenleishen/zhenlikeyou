776b1975f5bc726a69405491e677fbc84bd130e0
zhenleishen <123748596+zhenleishen@users.noreply.github.com>
2023-03-05 15:00:49 +0800

    Add files via upload

---
+++ js/verification.js
@@ -0,0 +1,15 @@
+$('#login-button').click(function (event) {
+    let userName = document.getElementById("userName").value;
+    let pwd = document.getElementById("pwd").value;
+    if (userName == "陆颖欣" && pwd == "20070311") {
+        $('#h').text("生日快乐！");
+        event.preventDefault();
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+        setTimeout(function () {
+            location.href = "BirthdayCake.html";
+        }, 4000);
+    } else {
+        alert("用户名或密码不正确！");
+    }
+});
