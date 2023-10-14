b142a03586868849e47477db2f863ac6f33529d5
zhenleishen <123748596+zhenleishen@users.noreply.github.com>
2023-10-15 00:22:20 +0800

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -1,8 +1,8 @@
 $('#login-button').click(function (event) {
     let userName = document.getElementById("userName").value;
     let pwd = document.getElementById("pwd").value;
-    if (userName == "2006" && pwd == "09") {
-        $('#h').text("欢迎来到振哥小黑屋");
+    if (userName == "01" && pwd == "06") {
+        $('#h').text("欢迎加入小疯队");
         event.preventDefault();
         $('form').fadeOut(100);
         $('.wrapper').addClass('form-success');
